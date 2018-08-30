f9480ec28f9861bf8a0d7dabf295a9d52b50b69d
Guillaume Chau <alphadelta.fg@gmail.com>
2018-08-30 23:18:11 +0200

    fix(ui): task start

--- ui.js
+++ ui.js
@@ -1,8 +1,5 @@
 module.exports = api => {
-  api.describeTask({
-    match: /vue-cli-service ssr:serve/,
-    description: `Compiles and hot-reloads for development with SSR`,
-    link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
+  const taskCommon = {
     prompts: [
       {
         name: 'port',
@@ -14,17 +11,25 @@ module.exports = api => {
     onBeforeRun: ({ answers, args }) => {
       if (answers.port) args.push('--port', answers.port)
     },
+  }
+
+  api.describeTask({
+    match: /vue-cli-service ssr:serve --mode production/,
+    description: `Starts the included HTTP server for SSR in production`,
+    link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
+    ...taskCommon,
+  })
+
+  api.describeTask({
+    match: /vue-cli-service ssr:serve/,
+    description: `Compiles and hot-reloads for development with SSR`,
+    link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
+    ...taskCommon,
   })
 
   api.describeTask({
     match: /vue-cli-service ssr:build/,
     description: `Compiles and minifies for production with SSR`,
-    link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
-  })
-
-  api.describeTask({
-    match: /vue-cli-service ssr:start/,
-    description: `Starts the included HTTP server for SSR in production`,
     link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
   })
 }
