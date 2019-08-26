7c8dd745bcc8bf7bfb52b16784ecf7b0cf2e4066
Marcel Jahn <marcel.jahn@sumcumo.com>
2019-08-26 11:20:39 +0200

    feat(updating_eslint): updating dependencies

--- ui.js
+++ ui.js
@@ -15,21 +15,21 @@ module.exports = api => {
 
   api.describeTask({
     match: /vue-cli-service ssr:serve --mode production/,
-    description: `Starts the included HTTP server for SSR in production`,
+    description: 'Starts the included HTTP server for SSR in production',
     link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
     ...taskCommon,
   })
 
   api.describeTask({
     match: /vue-cli-service ssr:serve/,
-    description: `Compiles and hot-reloads for development with SSR`,
+    description: 'Compiles and hot-reloads for development with SSR',
     link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
     ...taskCommon,
   })
 
   api.describeTask({
     match: /vue-cli-service ssr:build/,
-    description: `Compiles and minifies for production with SSR`,
+    description: 'Compiles and minifies for production with SSR',
     link: 'https://github.com/Akryum/vue-cli-plugin-ssr#usage',
   })
 }
